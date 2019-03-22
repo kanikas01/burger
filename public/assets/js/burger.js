5bc0a8bd8ee53b39315bb235d0830f9db3b2c1e2
kanikas01 <larvakurt@yahoo.com>
2019-03-22 12:13:06 -0700

    Move page javascript to external file

---
+++ public/assets/js/burger.js
@@ -0,0 +1,47 @@
+$(function() {
+  $(".create-form").on("submit", function(event) {
+    // Make sure to preventDefault on a submit event.
+    event.preventDefault();
+  
+    var newBurger = {
+      burger_name: $("#burger_name").val().trim()
+    };
+    if (!newBurger.burger_name) {
+      alert("Burger name cannot be blank.");
+      return;
+    }
+  
+    // Send the POST request.
+    $.ajax("/api/burgers", {
+      type: "POST",
+      data: newBurger
+    }).then(
+      function() {
+        console.log("created new burger");
+        // Reload the page to get the updated list
+        location.reload();
+      }
+    );
+  });
+  
+  $(".devour").on("click", function(event) {
+    var id = $(this).data("id");
+    var isDevoured = $(this).data("devoured");
+  
+    var newDevouredState = {
+      devoured: isDevoured
+    };
+  
+    // Send the PUT request.
+    $.ajax("/api/burgers/" + id, {
+      type: "PUT",
+      data: newDevouredState
+    }).then(
+      function() {
+        console.log("changed devoured to", isDevoured);
+        // Reload the page to get the updated list
+        location.reload();
+      }
+    );
+  });
+});
